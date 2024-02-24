9581a8525c0ebcb051b03670c467c93155c7d3b4
007-akash <aakashagrawal962@gmail.com>
2024-02-24 23:06:01 +0530

    added task2 and task3 for react

---
+++ react/assignment3/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import VideoCard from './components/VideoCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <VideoCard/>
+    </div>
+  );
+}
+
+export default App;
